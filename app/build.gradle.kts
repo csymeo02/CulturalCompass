addc87aa5137e1bff21dcb396da6f75fbf7a817f
csymeo02 <123817882+csymeo02@users.noreply.github.com>
2025-11-07 09:55:41 +0200

    commit files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
