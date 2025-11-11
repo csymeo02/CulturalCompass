d4cae528b4371f6c3c22cd255bb8e3b652a6c9de
csymeo02 <123817882+csymeo02@users.noreply.github.com>
2025-11-11 10:49:24 +0200

    Commit header & map setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.activity)
+    implementation(libs.google.location)
+    implementation(libs.google.maps)
+    implementation(libs.android.maps.utils)
 }
